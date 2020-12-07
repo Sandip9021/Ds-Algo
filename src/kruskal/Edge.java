3babcc071236dab028501c093e37568f28d22455
Sandip Pati <sandip9021@gmail.com>
2020-12-07 10:35:30 +0530

    Create Edge.java

---
+++ src/kruskal/Edge.java
@@ -0,0 +1,41 @@
+package kruskal;
+
+public class Edge implements Comparable<Edge>{
+	private int from;
+	private int to;
+	private int weight;
+	
+	public Edge(int from, int to, int weight) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.weight = weight;
+	}
+	
+	public int getFrom() {
+		return from;
+	}
+	public void setFrom(int from) {
+		this.from = from;
+	}
+	public int getTo() {
+		return to;
+	}
+	public void setTo(int to) {
+		this.to = to;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public int compareTo(Edge compareEdge) {
+		return this.weight - compareEdge.weight;
+	}
+	
+	
+
+}
