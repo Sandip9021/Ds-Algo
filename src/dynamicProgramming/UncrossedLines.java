65a710b90353227fbbca52ae6ea5fc0a76be8e12
Sandip Pati <sandip9021@gmail.com>
2021-06-02 17:07:26 +0530

    Create UncrossedLines.java

---
+++ src/dynamicProgramming/UncrossedLines.java
@@ -0,0 +1,20 @@
+class UncrossedLines {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n=nums1.length +1;
+         int m=nums2.length + 1;
+        
+        int dp[][] = new int[n][m];
+
+        for(int i=1; i<n; i++){
+            for(int j=1 ;j<m; j++){
+                if(nums1[i-1] == nums2[j-1]){
+                      dp[i][j]=dp[i-1][j-1]+1 ;
+                }else{
+                      dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[n-1][m-1];
+    }
+}
