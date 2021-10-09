41589b135d46f9008a7fb840096c1f4a7a077df5
Sandip Pati <sandip9021@gmail.com>
2021-10-09 09:10:54 +0530

    Create Subset2.java

---
+++ src/backTracking/Subset2.java
@@ -0,0 +1,22 @@
+class Subset2 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+           List<List<Integer>> result = new ArrayList<>();
+         dfs(result,new ArrayList<>(),0,nums);
+         return result;
+    }
+    
+     public void dfs(List<List<Integer>> result,List<Integer> li,int start,int[] nums){
+        result.add(new ArrayList(li));  
+         
+        if(li.size()==nums.length){
+               return;  
+         }  
+        
+         for(int i=start;i<nums.length;i++){
+              
+              li.add(nums[i]);
+              dfs(result,li,i+1,nums);
+              li.remove(li.size()-1);
+         }
+    }
+}
